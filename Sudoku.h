99b286e37cfd7e9c51577fa35b0576f1b877105a
choi9798 <wai9798tw@gmail.com>
2016-04-01 14:10:56 +0800

    ver 1.2

---
+++ Sudoku.h
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+
+class Sudoku {
+	public:
+		Sudoku();
+		void giveQuestion();
+		void readIn();
+		void solve();
+		void changeNum(int a, int b);
+		void changeRow(int a, int b);
+		void changeCol(int a, int b);
+		void rotate(int n);
+		void flip(int n);
+		void transform();
+		bool checkUnity(int arr[]);
+		bool isCorrect();
+		bool nozero();
+		void zero_position();
+		void possible(int arr[][9]);
+		void backtrack();
+	private:
+		int map[9][9];
+		int real_map[9][9];
+		int ans[9][9];
+		int possible_num[9];
+		int zero_posRow[9];
+		int zero_posCol[9];
+		int answer = 0;
+		int index;
+};
